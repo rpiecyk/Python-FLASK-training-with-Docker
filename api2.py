7a4ca6358e7b97bfc8bd53e5996d346f3abca0c2
Your Name <you@example.com>
2021-04-05 16:15:08 +0200

    disabled debugging

--- api2.py
+++ api2.py
@@ -4,6 +4,7 @@ from flask_restplus import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 @api.route('/api/v1/info', methods=['GET'])
 class ReturnInfo(Resource):
@@ -11,6 +12,6 @@ class ReturnInfo(Resource):
         return jsonify(Receiver='Hello api.py!')
 
 if __name__ == '__main__':
-    app.run(port=8080,debug=True, host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0')
 
 
